f05dc9b8b630a286b65deb764de7c2d07264f8b0
prongbang <prongbang@gmail.com>
2024-04-19 15:18:52 +0700

    initial: add example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## 1.0.0
 
+- Add example
+
+## 1.0.0
+
 - Key exchange: X25519
 - Encryption: XChaCha20
 - Authentication: Poly1305
---
+++ example/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:lazyxchacha/keypair.dart';
+import 'package:lazyxchacha/lazyxchacha.dart';
+
+void main() async {
+  final lazyxchacha = LazyXChaCha.instance;
+
+  // Generate KeyPair
+  final clientKeyPair = await KeyPair.newKeyPair();
+  final serverKeyPair = await KeyPair.newKeyPair();
+
+  // Key Exchange
+  final clientSharedKey = await clientKeyPair.sharedKey(serverKeyPair.pk);
+  final serverSharedKey = await serverKeyPair.sharedKey(clientKeyPair.pk);
+
+  // Payload
+  const message = 'Hello lazyxchacha';
+
+  // Encrypt with client
+  final ciphertext = await lazyxchacha.encrypt(message, clientSharedKey);
+
+  // Decrypt with server
+  final plaintext = await lazyxchacha.decrypt(ciphertext, serverSharedKey);
+
+  // Output
+  if (kDebugMode) {
+    print('Output: $plaintext');
+  } // Output: Hello lazyxchacha
+}
